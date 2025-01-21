6a57c69c2fe9728bd8004d151617d5701e526522
JoshVilla <villaearljoshua@gmail.com>
2025-01-21 16:00:48 +0800

    add logout function

--- frontend/src/layout/CustomerLayout/index.tsx
+++ frontend/src/layout/CustomerLayout/index.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { IoIosSunny, IoIosMoon } from "react-icons/io";
 import DefaultAvatar from "@/assets/user-avatar.png";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import { keepOneKeyAndRemoveOthers } from "@/utility/helpers";
 type Props = {};
 
 const CustomerLayout = (props: Props) => {
+  const navigate = useNavigate();
   const accountInfo = useSelector(
     (state: RootState) => state.userInfo.userInfo
   );
+
+  console.log(Object.keys(localStorage));
   const [theme, setTheme] = useState<string>(() => {
     // Check if there's a theme in localStorage or fall back to 'light-theme'
     return localStorage.getItem("theme") || "light-theme";
@@ -31,49 +35,63 @@ const CustomerLayout = (props: Props) => {
     localStorage.setItem("theme", theme);
   }, [theme]);
 
-  return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <div className="w-64 bg-gray-800 text-white flex flex-col">
-        <div className="p-4 text-2xl font-bold">Shop Sphere</div>
-        <ul className="flex flex-col p-4 space-y-4">
-          <li className="flex items-center gap-5">
-            <img src={DefaultAvatar} alt="" className="w-10 h-10 rounded-lg" />
-            <a href="/" className="hover:text-gray-400 cur">
-              Hello, {accountInfo?.username}
-            </a>
-          </li>
-          <li>
-            <a href="/" className="hover:text-gray-400">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/products" className="hover:text-gray-400">
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="/cart" className="hover:text-gray-400">
-              Cart
-            </a>
-          </li>
-          <li>
-            <a href="/profile" className="hover:text-gray-400">
-              Profile
-            </a>
-          </li>
-        </ul>
-      </div>
+  const logout = () => {
+    keepOneKeyAndRemoveOthers("theme");
+    navigate("/");
+  };
 
-      {/* Main Content */}
-      <div className="flex-1 bg-gray-100">
-        <div className="bg-border px-8 py-4 flex justify-between items-center">
-          <button onClick={toggleTheme} className="text-text text-xl">
-            {theme === "light-theme" ? <IoIosSunny /> : <IoIosMoon />}
-          </button>
+  return (
+    <div>
+      <div className="flex h-screen">
+        {/* Sidebar */}
+        <div className="w-64 bg-gray-800 text-white flex flex-col">
+          <div className="p-4 text-2xl font-bold">Shop Sphere</div>
+          <ul className="flex flex-col p-4 space-y-4">
+            <li className="flex items-center gap-5">
+              <img
+                src={DefaultAvatar}
+                alt=""
+                className="w-10 h-10 rounded-lg"
+              />
+              <a href="/" className="hover:text-gray-400 cur">
+                Hello, {accountInfo?.username}
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-gray-400">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/products" className="hover:text-gray-400">
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="/cart" className="hover:text-gray-400">
+                Cart
+              </a>
+            </li>
+            <li>
+              <a href="/profile" className="hover:text-gray-400">
+                Profile
+              </a>
+            </li>
+          </ul>
         </div>
-        <Outlet />
+
+        {/* Main Content */}
+        <div className="flex-1 bg-gray-100">
+          <div className="bg-border px-8 py-4 flex justify-between items-center">
+            <button onClick={toggleTheme} className="text-text text-xl">
+              {theme === "light-theme" ? <IoIosSunny /> : <IoIosMoon />}
+            </button>
+            <button className="text-text" onClick={logout}>
+              Logout
+            </button>
+          </div>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- frontend/src/utility/helpers.tsx
+++ frontend/src/utility/helpers.tsx
@@ -13,20 +13,32 @@ export const validatePassword = (password: string) => {
   const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
 
   if (password.length < minLength) {
-      messages.push(`Password must be at least ${minLength} characters long.`);
+    messages.push(`Password must be at least ${minLength} characters long.`);
   }
   if (!uppercaseRegex.test(password)) {
-      messages.push("Password must contain at least one uppercase letter.");
+    messages.push("Password must contain at least one uppercase letter.");
   }
   if (!lowercaseRegex.test(password)) {
-      messages.push("Password must contain at least one lowercase letter.");
+    messages.push("Password must contain at least one lowercase letter.");
   }
   if (!digitRegex.test(password)) {
-      messages.push("Password must contain at least one number.");
+    messages.push("Password must contain at least one number.");
   }
   if (!specialCharRegex.test(password)) {
-      messages.push("Password must contain at least one special character.");
+    messages.push("Password must contain at least one special character.");
   }
 
   return messages;
-}
+};
+
+// Function to maintain one key and remove all others from localStorage
+export const keepOneKeyAndRemoveOthers = (keyToKeep: string) => {
+  const allKeys = Object.keys(localStorage);
+
+  // Loop through all keys and remove them except for the keyToKeep
+  allKeys.forEach((key) => {
+    if (key !== keyToKeep) {
+      localStorage.removeItem(key);
+    }
+  });
+};
