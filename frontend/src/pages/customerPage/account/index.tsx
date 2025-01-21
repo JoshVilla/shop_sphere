91298eca778e85d8eef5ce869d16d026b4ddd4d5
JoshVilla <villaearljoshua@gmail.com>
2025-01-21 20:19:11 +0800

    add form in my account page

--- frontend/src/pages/customerPage/account/index.tsx
+++ frontend/src/pages/customerPage/account/index.tsx
@@ -1,29 +1,99 @@
 import Input from "@/components/input";
-import React from "react";
+import { RootState } from "@/store/store";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-type Props = {};
+interface FormStateProps {
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+}
 
-const Account = (props: Props) => {
+const Account = () => {
+  const info = useSelector((state: RootState) => state.userInfo.userInfo);
+  const [formState, setFormState] = useState<FormStateProps>({
+    username: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+
+  const saveAccount = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault(); // Prevent the button's default behavior
+    console.log(formState);
+  };
+
+  useEffect(() => {
+    setFormState({
+      username: info?.username || "",
+      firstname: info?.firstname || "",
+      lastname: info?.lastname || "",
+      email: info?.email || "",
+    });
+  }, []);
+
   return (
-    <div className="h-[100vh] bg-background text-text">
+    <div className="h-screen bg-background text-text">
       <div className="w-full  p-8">
         <div className="text-2xl font-semibold">My Account</div>
         <div className="mt-10">
           <form action="" className="flex flex-col gap-10">
             <div>
-              <label htmlFor="">Username: </label>
-              <Input />
+              {/* <label>Username: </label> */}
+              <Input
+                placeholder="Enter Username"
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    username: e.target.value,
+                  }))
+                }
+              />
             </div>
 
             <div>
-              <label htmlFor="">Firstname: </label>
-              <Input />
+              {/* <label>Firstname: </label> */}
+              <Input
+                placeholder="Enter Firstname"
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    firstname: e.target.value,
+                  }))
+                }
+              />
             </div>
 
             <div>
-              <label htmlFor="">Lastname: </label>
-              <Input />
+              {/* <label>Lastname: </label> */}
+              <Input
+                placeholder="Enter Lastname"
+                onChange={(e) =>
+                  setFormState((prev) => ({
+                    ...prev,
+                    lastname: e.target.value,
+                  }))
+                }
+              />
             </div>
+
+            <div>
+              {/* <label>Email: </label> */}
+              <Input
+                placeholder="Enter Email"
+                onChange={(e) =>
+                  setFormState((prev) => ({ ...prev, email: e.target.value }))
+                }
+              />
+            </div>
+            <button
+              onClick={saveAccount}
+              type="submit"
+              className="bg-primary p-2 rounded-md"
+            >
+              Save
+            </button>
           </form>
         </div>
       </div>
